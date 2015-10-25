2e890d2c9e1120fcc3efd112d99b8d13d3d55f9d
Mark Perkins <markeperk@gmail.com>
2015-10-25 03:58:36 -0600

    formatting update

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,7 +15,7 @@ import scala.collection.mutable.ListBuffer
 class Application extends Controller {
 
   def index = Action {
-  	val message = List("Please upload a .har file. Thank you","")
+  	val message = List("Please upload a .har file. Thank you")
     Ok(views.html.index(message))
   }
 	def upload = Action(parse.multipartFormData) { request =>
@@ -39,24 +39,28 @@ class Application extends Controller {
 				 .get("onLoad").get.asInstanceOf[Double]
 			//Entry Values
 			val entryData = logMap.get("entries").get.asInstanceOf[List[Map[String, Any]]]	
-			var loadTime = 0.0
-			entryData.foreach(i => loadTime += i.get("time").get.asInstanceOf[Double])
-			val avgLatency = entryData.map(i => 
-					 i.get("time").get.asInstanceOf[Double] - 
-					 i.get("timings").get.asInstanceOf[Map[String, Any]]
-					  .get("receive").get.asInstanceOf[Double]
+			var loadTime = 0.0 //just trying foreach vs map
+					entryData.foreach(i => loadTime += i.get("time").get.asInstanceOf[Double])
+			val avgLatency = entryData.map( i => 
+				  i.get("time").get.asInstanceOf[Double] - 
+				  i.get("timings").get.asInstanceOf[Map[String, Any]]
+				   .get("receive").get.asInstanceOf[Double]
 				).sum / entryData.length
-			val avgSend = entryData.map(i => i.get("timings").get.asInstanceOf[Map[String, Any]]
+			val avgSend = entryData.map( i => 
+					i.get("timings").get.asInstanceOf[Map[String, Any]]
 					 .get("send").get.asInstanceOf[Double]
 				).sum / entryData.length
-			val avgWait = entryData.map(i => i.get("timings").get.asInstanceOf[Map[String, Any]]
+			val avgWait = entryData.map( i => 
+					i.get("timings").get.asInstanceOf[Map[String, Any]]
 					 .get("wait").get.asInstanceOf[Double]
 				).sum / entryData.length
-			val avgReceive = entryData.map(i => i.get("timings").get.asInstanceOf[Map[String, Any]]
+			val avgReceive = entryData.map(i => 
+					i.get("timings").get.asInstanceOf[Map[String, Any]]
 					 .get("receive").get.asInstanceOf[Double]
 				).sum / entryData.length
-			//Approaches to Size
-			val contentSize = entryData.map(i => i.get("response").get.asInstanceOf[Map[String, Any]]
+			//Approaches to Request Size
+			val contentSize = entryData.map(i => 
+					i.get("response").get.asInstanceOf[Map[String, Any]]
 					 .get("content").get.asInstanceOf[Map[String, Any]]
 					 .get("size").get.asInstanceOf[Double]
 				).sum		
@@ -69,24 +73,26 @@ class Application extends Controller {
 			//Entry Worst Offenders
 			val time = entryData.map(i => 
 					i.get("time").get.asInstanceOf[Double])
-			val contentSizeList = entryData.map(i => i.get("response").get.asInstanceOf[Map[String, Any]]
+			val contentSizeList = entryData.map(i => 
+					i.get("response").get.asInstanceOf[Map[String, Any]]
 					 .get("content").get.asInstanceOf[Map[String, Any]]
 					 .get("size").get.asInstanceOf[Double]
 				)
-			val name = entryData.map(i => 
-					(i.get("request").get.asInstanceOf[Map[String, Any]]
-					 .get("url").get.asInstanceOf[String]).substring(
-					 (i.get("request").get.asInstanceOf[Map[String, Any]]
-					 .get("url").get.asInstanceOf[String]).lastIndexOf("/") + 1,
-		 			 (i.get("request").get.asInstanceOf[Map[String, Any]]
-					 .get("url").get.asInstanceOf[String]).length)
-					)
+			val name = entryData.map { i => 
+					var req = i.get("request").get.asInstanceOf[Map[String, Any]]
+										 .get("url").get.asInstanceOf[String]
+					if (req.lastIndexOf('/') == req.length - 1) {
+							req = req.substring(0, req.length - 1)
+					}
+					req.substring(req.lastIndexOf("/") + 1, req.length)
+				}
 			val entryAllTime = name.zip(time).toMap
 			val entryAllSize = name.zip(contentSizeList).toMap
 			val entryTimeTen = entryAllTime.toSeq.sortWith(_._2 > _._2).take(10)
 			val entrySizeTen = entryAllSize.toSeq.sortWith(_._2 > _._2).take(10)
 			//Entry Content-Type
-			val contentType = entryData.map(i => i.get("response").get.asInstanceOf[Map[String, Any]]
+			val contentType = entryData.map(i => 
+					i.get("response").get.asInstanceOf[Map[String, Any]]
 					 .get("content").get.asInstanceOf[Map[String, Any]]
 					 .get("mimeType").get.asInstanceOf[String]
 				)
@@ -106,7 +112,7 @@ class Application extends Controller {
 					 i.get("response").get.asInstanceOf[Map[String, Any]]
 					 	.get("cookies").get.asInstanceOf[List[String]].length 
 				).sum
-			//DB.save(numOfCookies) //wrong Type to save data - work on this later
+			//DB.save(numOfCookies) //wrong "Type" to save data - will work on this later
 			//Data List
 			var dataList = new ListBuffer[String]()
 					dataList += "Filename: " + har.filename
@@ -118,7 +124,7 @@ class Application extends Controller {
 					dataList += "Total Requests: " + entryData.length
 					dataList += "Transferred (Entry-Size): " + formatBytes(entrySize)
 					dataList += "Transferred (Content-Size): " + formatBytes(contentSize)
-					dataList += "- Avg. Transfer Size: " + convertTime(contentSize/entryData.length)
+					dataList += "- Avg. Transfer Size: " + formatBytes(contentSize/entryData.length)
 					dataList += "Duration (Total): " + convertTime(loadTime)
 					dataList += "- Avg. Duration: " + convertTime(loadTime/entryData.length)
 					dataList += "- Avg. Latency: " + convertTime(avgLatency)
@@ -128,16 +134,16 @@ class Application extends Controller {
 					dataList += "Number of Cookies: " + numOfCookies
 					dataList += "--------"
 					dataList += "Number of Content-Types:"
-					for((k,v) <- contentTypeCount) dataList += ("- " + k + " (" + v + ")")
+						for((k,v) <- contentTypeCount) dataList += ("- " + k + " (" + v + ")")
 					dataList += "--------"
 					dataList += "Number of Response Status-Types: "
-					for((k,v) <- statusTypeCount) dataList += ("- " + k + " (" + v + ")")
+						for((k,v) <- statusTypeCount) dataList += ("- " + k + " (" + v + ")")
 					dataList += "--------"
 					dataList += "Worst Offenders (by LoadTime): "
-					for((k,v) <- entryTimeTen) dataList += "- Duration: " + convertTime(v) + "; Name: " + k
+						for((k,v) <- entryTimeTen) dataList += "- Duration: " + convertTime(v) + "; Name: " + k
 					dataList += "--------"
 					dataList += "Worst Offenders (by Size): "
-					for((k,v) <- entrySizeTen) dataList += "- Size: " + formatBytes(v) + "; Name: " + k
+						for((k,v) <- entrySizeTen) dataList += "- Size: " + formatBytes(v) + "; Name: " + k
 			val dataForDisplay = dataList.toList
 	    Ok(views.html.index(dataForDisplay))
 	  }.getOrElse {
@@ -152,7 +158,7 @@ class Application extends Controller {
 	}
   def convertTime(args: Double) : String = {
 		import java.text.SimpleDateFormat;
-		return new SimpleDateFormat("s.SSSS").format(args) + " sec"
+		return new SimpleDateFormat("s.SSS").format(args) + " ms"
   }
   def formatBytes(args: Double) : String = {
   	import java.text.DecimalFormat
